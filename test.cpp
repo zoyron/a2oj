5c682a439e3f0552f59613cf16ce41a1e3d52b66
zoyron <sgrarora3@gmail.com>
2021-11-29 15:32:35 +0530

    added a test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define endl '\n'
+
+using namespace std;
+
+void solve(){}
+
+int32_t main(){
+  int t = 1;
+  //cin >> t;
+  while(t--)
+    solve();
+  return 0;
+}
