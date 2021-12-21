6191b3583cd1fcba79fc6fa153ad132fdb615993
zoyron <sgrarora3@gmail.com>
2021-12-21 23:51:03 +0530

    minor update

---
+++ aladder/getline.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define     endl '\n'
+#define     mod int(1e9) + 7
+using namespace std;
+
+void solve(){
+  string s;
+  getline(cin, s);
+  cout << s << endl;
+}
+
+int32_t main(){
+  int t = 1;
+  cin >> t;
+  while(t--)
+    solve();
+  return 0;
+}
+
+
